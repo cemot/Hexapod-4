b95b0e6098d6aa9134d27adf99a8a181069c9fd1
doostveen <dcvanoostveen@gmail.com>
2015-06-24 15:52:27 +0200

    Model changes

--- Hexapod/Hexapod.ino
+++ Hexapod/Hexapod.ino
@@ -4,10 +4,12 @@
 #include "ServoControl.h";
 #include "Movement.h";
 #include <SoftwareSerial.h>
+#include "MovementController.h";
 
 String command = ""; // Stores response of the HC-06 Bluetooth device
 int ledPin = 13;
 SoftwareSerial mySerial(10, 11); // RX, TX
+MovementVector movementVector;
 
 void setup() {
   Serial.begin(9600);
@@ -22,51 +24,81 @@ void setup() {
 
 void loop() {
   // Read device output if available.
-   if (mySerial.available()) {
-    while(mySerial.available()) { // While there is more to be read, keep reading.
+  if (mySerial.available()) {
+    while (mySerial.available()) { // While there is more to be read, keep reading.
       command += (char)mySerial.read();
       delay(10);
     }
 
     ParseCommand(command);
     command = ""; // No repeats
-   }
-   
-  // put your main code here, to run repeatedly:
-  delay(10);
+  }
+
+  // handle movement vector
+  // send to MovementController
+  
+  // check movement interval
+  delay(100);
 }
 
-void ParseCommand(String command){
+void ParseCommand(String command) {
   String response = "";
   digitalWrite(13, LOW);
   bool stopMovement = true;
-  if(command == "?" || command == "")
+  if (command == "?" || command == "")
   {
-    response +="Commands:\r\n";
-    response +="f: forward\r\n";
-    //response +="b: backward\r\n";
-    //response +="l: left\r\n";
-    //response +="r: right\r\n";
-    response +="s: stop\r\n";
-    response +="x: light led 13\r\n";
-    response +="?: display this help\r\n";
+    response += "Commands:\r\n";
+    response += "f: forward\r\n";
+    response += "b: backward\r\n";
+    response += "l: left\r\n";
+    response += "r: right\r\n";
+    response += "s: stop\r\n";
+    response += "x: light led 13\r\n";
+    response += "?: display this help\r\n";
   }
-  else if(command == "x")
+  else if (command == "x")
   {
     response = "Lights on!";
-    digitalWrite(13, HIGH);  
+    digitalWrite(13, HIGH);
   }
-  else if(command == "f")
+  else if (command == "f")
   {
     response = "Forward";
+    movementVector.SetSurge(1.0);
     ServoMovement(StraightMovement);
   }
-  else if (command == "s")
+  else if (command == "b")
+  {
+    response = "Backward";
+    movementVector.SetSurge(-1.0);
+    ServoMovement(StraightMovement);
+  }
+  else if (command == "l")
+  {
+    response = "Yaw left";
+    movementVector.SetYaw(1.0);
+    ServoMovement(StraightMovement);
+  }
+  else if (command == "r")
+  {
+    response = "Yaw right";
+    movementVector.SetYaw(-1.0);
+    ServoMovement(StraightMovement);
+  }  else if (command == "s")
   {
     stopMovement = true;
     response = "Stop";
+    movementVector.SetStop();
   }
-  
-  response +="\r\n";
+  else if (command == "p")
+  {
+    String p = String("#MV:S:2.5;Y:0.0;H:0.0;r:1.2;p:0.0;y:-1.5;");
+    movementVector.Parse(p);
+  }
+
+  response += "\r\n";
   mySerial.println(response);
+  mySerial.print("Movement Vector : ");
+  mySerial.println(movementVector.ToString());
 }
+
---
+++ Hexapod/MovementController.h
@@ -0,0 +1,126 @@
+/*
+ * Class that handles the movement vector and updates the Servo's
+ */
+
+class MovementController
+{
+    MovementController() {};
+
+    ~MovementController() {};
+};
+
+/*
+ * Class defines an actual movement state for the hexapod body
+ */
+class MovementVector
+{
+  public:
+
+    MovementVector()
+    {
+    }
+
+    ~MovementVector()
+    {
+    }
+
+    void SetSurge(double f)
+    {
+      this->surge += f;
+    }
+
+    void SetYaw(double y)
+    {
+      this->yaw += y;
+    }
+
+    void SetStop()
+    {
+      this->surge = 0;
+      this->sway = 0;
+      this->heave = 0;
+      this->roll = 0;
+      this->pitch = 0;
+      this->yaw = 0;
+    }
+
+    String ToString()
+    {
+      String s = "#MV:S:";
+      s += String(this->surge, 1);
+      s += ";Y:";
+      s += String(this->sway, 1);
+      s += ";H:";
+      s += String(this->heave, 1);
+      s += ";r:";
+      s += String(this->roll, 1);
+      s += ";p:";
+      s += String(this->pitch, 1);
+      s += ";y:";
+      s += String(this->yaw, 1);
+      s += ";";
+      return s;
+    }
+
+    // reads values from input string
+    void Parse(String value)
+    {
+      if (!value.startsWith("#MV:S:"))
+      {
+        return;
+      }
+
+      int readPosition = 4;
+      int stringLength = value.length();
+      while (readPosition >= 0 && readPosition < stringLength)
+      {
+        // find field
+        char cmd = value.charAt(readPosition);
+
+        // find : semicolon
+        int dataStartIndex = value.indexOf(":", readPosition);
+
+        // read double value between : and #
+        int fieldTerminatorPosition = value.indexOf(";", readPosition);
+        String v = value.substring(dataStartIndex + 1, fieldTerminatorPosition - 1);
+
+        // write info field
+        double f = v.toFloat();
+        switch (cmd) {
+          case 'S':
+            this->surge = f;
+            break;
+          case 'Y':
+            this->sway = f;
+            break;
+          case 'H':
+            this->heave = f;
+            break;
+          case 'r':
+            this->roll = f;
+            break;
+          case 'p':
+            this->pitch = f;
+            break;
+          case 'y':
+            this->yaw = f;
+            break;
+          default:
+            break;
+        }
+
+        // truncate remaining string
+        readPosition = fieldTerminatorPosition + 1;
+      }
+    }
+
+    double surge;
+    double sway;
+    double heave;
+    double roll;
+    double pitch;
+    double yaw;
+
+  private:
+};
+
